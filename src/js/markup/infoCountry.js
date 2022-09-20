5745618a457c510bb8149ed83f1dcb6e29b0651a
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-09-20 10:03:42 +0300

    Update

--- src/js/markup/infoCountry.js
+++ src/js/markup/infoCountry.js
@@ -5,13 +5,15 @@
 
   refs.countryList.innerHTML = '';
 
-  refs.countryInfo.innerHTML = `<div class="country-info">
+   refs.countryInfo.innerHTML = `<div class="country-info">
+  <div class="main-info">
   <img
     src="${flags.png}"
     alt="flag of ${name.official}"
     class="country-info_flag"
   />
   <h2 class="country-info_name">${name.official}</h2>
+  </div>
   <ul class="country-info_list">
     <li class="country-info_list--capital">
       <span class="country-info_list--capital-text">Capital: </span>
@@ -21,7 +23,7 @@
       <span class="country-info_population-link">${population}</span>
     </li>
     <ul class="languages">
-      <li class="language_text">Language: ${languages}</li>
+      <li class="language_text">Language: <span class="languages"> ${languages} </span></li>
     </ul>
   </ul>
 </div>`;
